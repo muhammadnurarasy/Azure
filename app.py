22ffc7827d30d6887f3ebededf0891b11c53ac0e
muhammadnurarasy <120718183+muhammadnurarasy@users.noreply.github.com>
2023-09-14 12:59:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import joblib
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 # Load the model
 model = joblib.load('finalized_model.sav')
 
 @app.route('/')
 def index():
-    return "Welcome to the House Price Predictor!"
+    return render_template("index.html")
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
     prediction = model.predict([data['features']])
     return jsonify(prediction=float(prediction[0]))
+
+if __name__ == "__main__":
+    app.run(debug=True)
