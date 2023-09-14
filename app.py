61a1550e037327595fb7ed94b9ed6f3e77bb0708
muhammadnurarasy <120718183+muhammadnurarasy@users.noreply.github.com>
2023-09-14 13:42:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, jsonify, request, render_template
 import joblib
 
-app = Flask(__name__, static_folder="static", template_folder="templates")
+app = Flask(__name__, static_folder=".", static_url_path="", template_folder=".")
 
 # Load the model
 model = joblib.load('finalized_model.sav')
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -16,5 +16,9 @@ def predict():
     prediction = model.predict([data['features']])
     return jsonify(prediction=float(prediction[0]))
 
-if __name__ == "__main__":
+@app.route('/<path:path>')
+def serve_static(path):
+    return app.send_static_file(path)
+
+if __name__ == '__main__':
     app.run(debug=True)
