b26edc0dad2797ec5d41cade6dfc9751219a8915
muhammadnurarasy <120718183+muhammadnurarasy@users.noreply.github.com>
2023-09-14 13:50:52 +0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -2,30 +2,22 @@ document.addEventListener("DOMContentLoaded", function () {
     // When the "Predict Price" button is clicked
     document.getElementById("predict-btn").addEventListener("click", function () {
         // Gather data from input fields
-        let features = {
-            document.getElementById('predict-btn').addEventListener('click', function() {
-    const neighborhoodMean = parseFloat(document.getElementById('neighborhood-mean').value);
-    const houseType = document.getElementById('house-type').value;
-    const overallQuality = parseInt(document.getElementById('overall-quality').value);
-    const firstFloorSqft = parseInt(document.getElementById('first-floor-sqft').value);
-    const roomsAboveGrade = parseInt(document.getElementById('rooms-above-grade').value);
-    const yearBuilt = parseInt(document.getElementById('year-built').value);
-    const fullBathrooms = parseInt(document.getElementById('full-bathrooms').value);
+        const neighborhoodMean = parseFloat(document.getElementById('neighborhood-mean').value);
+        const houseType = document.getElementById('house-type').value;
+        const overallQuality = parseInt(document.getElementById('overall-quality').value);
+        const firstFloorSqft = parseInt(document.getElementById('first-floor-sqft').value);
+        const roomsAboveGrade = parseInt(document.getElementById('rooms-above-grade').value);
+        const yearBuilt = parseInt(document.getElementById('year-built').value);
+        const fullBathrooms = parseInt(document.getElementById('full-bathrooms').value);
 
-    const features = {
-        neighborhoodMean: neighborhoodMean,
-        houseType: houseType,
-        overallQuality: overallQuality,
-        firstFloorSqft: firstFloorSqft,
-        roomsAboveGrade: roomsAboveGrade,
-        yearBuilt: yearBuilt,
-        fullBathrooms: fullBathrooms
-    };
-
-    // Use the features to make an API call to your Flask backend
-    // ... (the rest of your predict price function)
-});
-
+        const features = {
+            neighborhoodMean: neighborhoodMean,
+            houseType: houseType,
+            overallQuality: overallQuality,
+            firstFloorSqft: firstFloorSqft,
+            roomsAboveGrade: roomsAboveGrade,
+            yearBuilt: yearBuilt,
+            fullBathrooms: fullBathrooms
         };
 
         // Make an AJAX call to the Flask backend
