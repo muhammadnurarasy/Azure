0d9bc10467a4a2190638e24a4155a14f4dcbf7f1
muhammadnurarasy <120718183+muhammadnurarasy@users.noreply.github.com>
2023-09-14 13:48:30 +0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,31 +1,48 @@
-document.getElementById("predictForm").addEventListener("submit", async function(e) {
-    e.preventDefault();
-    
-    // Extract values
-    let OverallQual = document.getElementById("OverallQual").value;
-    let GrLivArea = document.getElementById("GrLivArea").value;
-    let GarageCars = document.getElementById("GarageCars").value;
-    let GarageArea = document.getElementById("GarageArea").value;
-    let TotalBsmtSF = document.getElementById("TotalBsmtSF").value;
+document.addEventListener("DOMContentLoaded", function () {
+    // When the "Predict Price" button is clicked
+    document.getElementById("predict-btn").addEventListener("click", function () {
+        // Gather data from input fields
+        let features = {
+            document.getElementById('predict-btn').addEventListener('click', function() {
+    const neighborhoodMean = parseFloat(document.getElementById('neighborhood-mean').value);
+    const houseType = document.getElementById('house-type').value;
+    const overallQuality = parseInt(document.getElementById('overall-quality').value);
+    const firstFloorSqft = parseInt(document.getElementById('first-floor-sqft').value);
+    const roomsAboveGrade = parseInt(document.getElementById('rooms-above-grade').value);
+    const yearBuilt = parseInt(document.getElementById('year-built').value);
+    const fullBathrooms = parseInt(document.getElementById('full-bathrooms').value);
 
-    // Make a prediction request
-    let response = await fetch("https://arasy.z23.web.core.windows.net/", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            OverallQual: OverallQual,
-            GrLivArea: GrLivArea,
-            GarageCars: GarageCars,
-            GarageArea: GarageArea,
-            TotalBsmtSF: TotalBsmtSF
+    const features = {
+        neighborhoodMean: neighborhoodMean,
+        houseType: houseType,
+        overallQuality: overallQuality,
+        firstFloorSqft: firstFloorSqft,
+        roomsAboveGrade: roomsAboveGrade,
+        yearBuilt: yearBuilt,
+        fullBathrooms: fullBathrooms
+    };
+
+    // Use the features to make an API call to your Flask backend
+    // ... (the rest of your predict price function)
+});
+
+        };
+
+        // Make an AJAX call to the Flask backend
+        fetch("/predict", {
+            method: "POST",
+            body: JSON.stringify({ features: features }),
+            headers: {
+                "Content-Type": "application/json"
+            }
         })
+        .then(response => response.json())
+        .then(data => {
+            // Display the predicted price
+            document.getElementById("predicted-price").innerText = "Predicted Price: $" + data.prediction.toFixed(2);
+        })
+        .catch(error => {
+            console.error("Error:", error);
+        });
     });
-
-    let data = await response.json();
-    
-    // Display predicted price
-    document.getElementById("predictedPrice").innerText = "$" + data.prediction.toFixed(2);
-    document.getElementById("prediction").classList.remove("hidden");
 });
